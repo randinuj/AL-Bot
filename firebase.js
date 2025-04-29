94679f4f7f27fff3af6e3c3ad5b2ad7a6e3949dc
Randinu Jayaratne <108204447+randinuj@users.noreply.github.com>
2025-04-29 21:08:03 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,8 +1,14 @@
-// Import necessary functions from Firebase SDK
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-app.js";
-import { getFirestore, doc, getDoc, setDoc } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-firestore.js";
+import {
+  getFirestore,
+  doc,
+  getDoc,
+  setDoc,
+  updateDoc,
+  arrayUnion,
+  arrayRemove
+} from "https://www.gstatic.com/firebasejs/11.6.1/firebase-firestore.js";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyC99x-h0rsxYd_uqu3_axIyI9p8Qdz_MXY",
   authDomain: "randinu-al-helper.firebaseapp.com",
@@ -13,9 +19,7 @@ const firebaseConfig = {
   appId: "1:890408425238:web:81dd575aa1e0b2c341e237"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app); // Get Firestore instance
+const db = getFirestore(app);
 
-// Export db so it can be used in other parts of the app
-export { db, doc, getDoc, setDoc };
+export { db, doc, getDoc, setDoc, updateDoc, arrayUnion, arrayRemove };
